5067f42bd7e1e319ee5911c28a682489602c5bfb
PehMarquesDev <pehmarquesDev@gmail.com>
2023-05-16 15:53:39 -0300

    Microserviço de consulta se recupera após falha

--- barramento-de-eventos/index.js
+++ barramento-de-eventos/index.js
@@ -6,21 +6,34 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+const eventos = []
+
 // Esse post irá enviar o evento como conteúdo para os serviços presentes
 // Ele recebe o evento que foi enviado por algum dos outros serviços
-app.post('/eventos', (req, res) => {
+app.post('/eventos', async (req, res) => {
     const evento = req.body;
+    eventos.push(evento)
     //console.log(req.body);
-    //envia o evento para o microsserviço de lembretes
-    axios.post('http://localhost:4000/eventos', evento);
-    //envia o evento para o microsserviço de observações
-    axios.post('http://localhost:5000/eventos', evento);
-    //envia o evento para o microsserviço de consulta
-    axios.post("http://localhost:6000/eventos", evento);
-    //envia o evento para o microsservico de classificacao
-    axios.post("http://localhost:7000/eventos", evento);
+    try {
+        //envia o evento para o microsserviço de lembretes
+        await axios.post('http://localhost:4000/eventos', evento);
+        //envia o evento para o microsserviço de observações
+        await axios.post('http://localhost:5000/eventos', evento);
+        //envia o evento para o microsserviço de consulta
+        await axios.post("http://localhost:6000/eventos", evento);
+        //envia o evento para o microsservico de classificacao
+        await axios.post("http://localhost:7000/eventos", evento);
+    }
+    catch (error) {
+        console.log("O serviço de Classificação está indisponível")
+    }
     res.status(200).send({ msg: "ok" });
 });
+
+app.get('/eventos', (req, res) => {
+    res.send(eventos)
+})
+
 
 app.listen(10000, () => {
     console.log('Barramento de eventos. Porta 10000.')
--- consulta/index.js
+++ consulta/index.js
@@ -2,6 +2,9 @@
 // Por isso não precisa do pacote axios
 
 const express = require("express");
+
+// adição do axios para requisitar eventos perdidos ao barramento
+const axios = require("axios");
 const app = express();
 app.use(express.json());
 
@@ -22,7 +25,7 @@ const funcoes = {
         const observacoes =
             baseConsulta[observacao.lembreteId]["observacoes"];
         const indice = observacoes.findIndex((o) => o.id ===
-        //findIndex retorna o indice onde ele encontrar o primeiro objeto que supre o parametro
+            //findIndex retorna o indice onde ele encontrar o primeiro objeto que supre o parametro
             observacao.id);
         observacoes[indice] = observacao;
     }
@@ -33,12 +36,21 @@ app.get("/lembretes", (req, res) => {
 });
 
 app.post("/eventos", (req, res) => {
-    try{
+    try {
         funcoes[req.body.tipo](req.body.dados);
     }
-    catch (err){
+    catch (err) {
         res.status(200).send(baseConsulta);
     }
 });
 
-app.listen(6000, () => console.log("Consultas. Porta 6000"));
+app.listen(6000, async () => {
+    console.log("Consultas. Porta 6000");
+    const resp = await axios.get("http://localhost:10000/eventos");
+    resp.data.forEach((valor, indice, colecao) => {
+        try {
+            funcoes[valor.tipo](valor.dados);
+        }
+        catch (err) { }
+    });
+});
